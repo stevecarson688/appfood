7975902a4514d19261cad2acb5f757356bedf1e3
stevecarson688 <105128928+stevecarson688@users.noreply.github.com>
2023-04-15 08:05:00 +0100

    igl1a

---
+++ index.js
@@ -0,0 +1,16 @@
+var small_menu = document.querySelector('.toggle_menu');
+var menu = document.querySelector('.menu');
+small_menu.onclick = function () {
+    small_menu.classList.toggle('active');
+    menu.classList.toggle('responsive');
+}
+
+
+
+
+
+
+
+
+
+
